34dd7edc957f76e5f9c6e225a069cb7f99279d1d
CRStromberg <cstromberg86@gmail.com>
2020-09-24 10:44:56 -0600

    Was looking at the wrong coverage map. Updated Push.

--- Objects/app/src/main/java/com/example/objects/Speaker.java
+++ Objects/app/src/main/java/com/example/objects/Speaker.java
@@ -1,54 +1,57 @@
 package com.example.objects;
 
 public class Speaker {
-    public Speaker(double size) {
-        if (size > 0.0) {
+    public Speaker(double size, double frequency) {
+        if(size > 0.0 && frequency > 0.0) {
             this.size = size;
-        } else {
-            String error = size + ": Size cannot be negative.";
+            this.frequency = frequency;
+        } else if(size < 0) {
+            String error = "Error, Invalid Size: " + size + "\nSize Must Be Positive.";
+            throw new IllegalArgumentException(error);
+        }else if(frequency < 0) {
+            String error = "Error, Invalid Frequency: " + size + "\nFrequency Must Be Positive.";
             throw new IllegalArgumentException(error);
         }
     }
 
-    //Property
     private double size;
-    //Method (getter)
+    private double frequency;
+    private String type;
+
     public double getSize() {
         return size;
     }
-    //Property
-    //For sake of simplicity I'm just using the top end of frequency response
-    private double frequency;
-    //Method (getter)
+
     public double getFrequency() {
         return frequency;
     }
 
-    //Property
-    private String type;
-    //Method (getter)
+    public void setType(double frequency) {
+        if (frequency >= 3000.0) {
+            this.frequency = frequency;
+            type = "Tweeter";
+        } else if (frequency >= 100.0) {
+            this.frequency = frequency;
+            type = "Midrange";
+        } else {
+            this.frequency = frequency;
+            type = "Woofer";
+        }
+    }
+
     public String getType() {
         return type;
     }
 
-    public boolean audible() {
-        return frequency >= 20.0;
+    public boolean audible;
+
+    public boolean isAudible() {
+        return frequency >=20;
     }
 
-    public void setType(double frequency) {
-        if (frequency >= 3000.0 && size > 0.0) {
-            this.frequency = frequency;
-            type = "Tweeter";
-        } else if (frequency >= 100.0 && size > 0.0) {
-            this.frequency = frequency;
-            type = "Midrange";
-        } else if (frequency >= 20.0 && size > 0.0) {
-            this.frequency = frequency;
-            type = "Woofer";
-        } else {
-            String error = frequency + ": Not a valid frequency.";
-            throw new IllegalArgumentException(error);
-        }
+    public void setAudible(boolean audible)
+    {
+        this.audible = audible;
     }
-
 }
+
--- Objects/app/src/test/java/com/example/objects/SpeakerTest.java
+++ Objects/app/src/test/java/com/example/objects/SpeakerTest.java
@@ -6,40 +6,111 @@ import static org.junit.Assert.*;
 
 public class SpeakerTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidSize() {
+        double invalidSize = -1.4;
+        double frequency = 100;
+        String error = "Error, Invalid Size: " + invalidSize + "\nSize Must Be Positive.";
+        try {
+            Speaker instance = new Speaker(invalidSize, frequency);
+        } catch (IllegalArgumentException ex) {
+            assertEquals(error, ex.getMessage());
+            throw ex;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidFrequency() {
+        double invalidSize = 1.4;
+        double frequency = -100;
+        String error = "Error, Invalid Frequency: " + invalidSize + "\nFrequency Must Be Positive.";
+        try {
+            Speaker instance = new Speaker(invalidSize, frequency);
+        } catch (IllegalArgumentException ex) {
+            assertEquals(error, ex.getMessage());
+            throw ex;
+        }
+    }
+
     @Test
-    public void getSize() {
+    public void GetSize() {
         double size = 6.5;
-        Speaker instance = new Speaker(size);
+        double frequency = 200;
+        Speaker instance = new Speaker(size, frequency);
         assertEquals(size, instance.getSize(), 6.5);
     }
 
     @Test
-    public void getFrequency() {
-        double frequency = 56.75;
-        Speaker instance = new Speaker(frequency);
-        assertEquals(frequency, instance.getFrequency(), 56.75);
+    public void GetFrequency() {
+        double size = 6.5;
+        double frequency = 200;
+        Speaker instance = new Speaker(size, frequency);
+        assertEquals(frequency, instance.getFrequency(), 200);
     }
 
     @Test
-    public void getType() {
-        Speaker instance = new Speaker(13.0);
-        assertEquals(null, instance.getType());
-    }
-
-    @Test
-    public void audibleTrue() {
-        double frequency = 674.87;
-        Speaker instance = new Speaker(frequency);
+    public void setTypeTweeter() {
+        double size = 6.5;
+        double frequency = 10900;
+        String type = "Tweeter";
+        Speaker instance = new Speaker(size, frequency);
         instance.setType(frequency);
-        assertEquals(true, instance.audible());
+        assertEquals(type, instance.getType(), "Tweeter");
     }
-
 
     @Test
-    public void setType() {
-        double frequency = 56.75;
-        Speaker instance = new Speaker(frequency);
+    public void setTypeMidrange() {
+        double size = 6.5;
+        double frequency = 900.88;
+        String type = "Midrange";
+        Speaker instance = new Speaker(size, frequency);
         instance.setType(frequency);
-        assertEquals("Woofer", instance.getType());
+        assertEquals(type, instance.getType(), "Midrange");
     }
+
+    @Test
+    public void setTypeWoofer() {
+        double size = 6.5;
+        double frequency = 87.32;
+        String type = "Woofer";
+        Speaker instance = new Speaker(size, frequency);
+        instance.setType(frequency);
+        assertEquals(type, instance.getType(), "Woofer");
+    }
+
+    @Test
+    public void GetType() {
+        double size = 6.5;
+        double frequency = 87.32;
+        String type = "Woofer";
+        Speaker instance = new Speaker(size, frequency);
+        assertEquals(null, instance.getType(), null);
+    }
+
+    @Test
+    public void isAudibleTrue() {
+        double size = 32.7;
+        double frequency = 298.43;
+        Speaker instance = new Speaker(size, frequency);
+        assertEquals(true, instance.isAudible());
+    }
+
+    @Test
+    public void isAudibleFalse() {
+        double size = 0.75;
+        double frequency = 9.45;
+        Speaker instance = new Speaker(size, frequency);
+        assertEquals(false, instance.isAudible());
+    }
+
+    @Test
+    public void setAudible() {
+        double size = 6.5;
+        double frequency = 87.32;
+        Speaker instance = new Speaker(size, frequency);
+        instance.setAudible(instance.isAudible());
+        assertEquals(true, instance.isAudible());
+        instance.setType(frequency);
+    }
+
 }
