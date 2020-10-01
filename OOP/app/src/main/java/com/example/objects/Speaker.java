c067f19a15646cb7359f5bbe1bda8ce3ada8a7bb
CRStromberg <cstromberg86@gmail.com>
2020-10-01 16:17:23 -0600

    Speaker.java updated

--- OOP/app/src/main/java/com/example/objects/Speaker.java
+++ OOP/app/src/main/java/com/example/objects/Speaker.java
@@ -1,57 +1,17 @@
 package com.example.objects;
 
-public class Speaker {
-    public Speaker(double size, double frequency) {
-        if(size > 0.0 && frequency > 0.0) {
-            this.size = size;
-            this.frequency = frequency;
-        } else if(size < 0) {
-            String error = "Error, Invalid Size: " + size + "\nSize Must Be Positive.";
-            throw new IllegalArgumentException(error);
-        }else if(frequency < 0) {
-            String error = "Error, Invalid Frequency: " + size + "\nFrequency Must Be Positive.";
-            throw new IllegalArgumentException(error);
-        }
+public interface Speaker {
+    double getSize();
+    void setSize(double size);
+
+    double getFrequency();
+    void setFrequency();
+
+    default boolean audible() {
+        return getFrequency() <= 20.0;
     }
 
-    private double size;
-    private double frequency;
-    private String type;
-
-    public double getSize() {
-        return size;
-    }
-
-    public double getFrequency() {
-        return frequency;
-    }
-
-    public void setType(double frequency) {
-        if (frequency >= 3000.0) {
-            this.frequency = frequency;
-            type = "Tweeter";
-        } else if (frequency >= 100.0) {
-            this.frequency = frequency;
-            type = "Midrange";
-        } else {
-            this.frequency = frequency;
-            type = "Woofer";
-        }
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public boolean audible;
-
-    public boolean isAudible() {
-        return frequency >=20;
-    }
-
-    public void setAudible(boolean audible)
-    {
-        this.audible = audible;
-    }
+    String getType();
+    void setType(double frequency);
 }
 
