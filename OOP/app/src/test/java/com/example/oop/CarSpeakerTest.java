247a184e91b47588f335f6dafbf317dc6ee7aae2
CRStromberg <cstromberg86@gmail.com>
2020-10-08 09:50:07 -0600

    CarSpeaker test created

---
+++ OOP/app/src/test/java/com/example/oop/CarSpeakerTest.java
@@ -0,0 +1,24 @@
+package com.example.oop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarSpeakerTest {
+
+    @Test
+    public void getSpeakerSize() {
+    }
+
+    @Test
+    public void setCarSpeaker() {
+    }
+
+    @Test
+    public void setAudible() {
+    }
+
+    @Test
+    public void audible() {
+    }
+}
