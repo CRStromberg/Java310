8755b11c28cc330261e17d46a50c7294448e38df
CRStromberg <cstromberg86@gmail.com>
2020-10-08 09:49:00 -0600

    CarSpeaker done

--- OOP/app/src/main/java/com/example/oop/CarSpeaker.java
+++ OOP/app/src/main/java/com/example/oop/CarSpeaker.java
@@ -1,7 +1,29 @@
 package com.example.oop;
 
-public class CarSpeaker extends DefaultSpeaker {
+public class CarSpeaker extends DefaultSpeaker{
 
-    public class CarSpeaker {
+    private boolean audible = false;
+
+    public CarSpeaker(double size){
+        super(size);
+    }
+
+    public CarSpeaker(double size, double frequency) {
+        super(size, frequency);
+    }
+
+    public double getSpeakerSize() {
+        return getSize();
+    }
+
+    public void setCarSpeaker(double speakerSize) {
+        setSize(speakerSize);
+    }
+
+    public void setAudible(boolean audible) {
+        this.audible = audible;
+    }
+    public boolean audible() {
+        return audible;
     }
 }
