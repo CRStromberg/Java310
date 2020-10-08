df3825560a8e3c3d52ab4edb26a4bbb5bb850a27
CRStromberg <cstromberg86@gmail.com>
2020-10-08 09:40:56 -0600

    Defualt test done

--- OOP/app/src/test/java/com/example/oop/DefaultSpeakerTest.java
+++ OOP/app/src/test/java/com/example/oop/DefaultSpeakerTest.java
@@ -47,18 +47,54 @@ public class DefaultSpeakerTest {
         double size = 6.5;
         double frequency = 87.32;
         DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
-        assertEquals(null, instance.getType(), null);
+        assertEquals(null, instance.getType(), "Woofer");
     }
 
     @Test
-    public void setType() {
+    public void setTypeTweeter() {
+        double size = 6.5;
+        double frequency = 10900;
+        String type = "Tweeter";
+        DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
+        instance.setType(frequency);
+        assertEquals(type, instance.getType(), "Tweeter");
+    }
+
+    @Test
+    public void setTypeMidrange() {
+        double size = 6.5;
+        double frequency = 900.88;
+        String type = "Midrange";
+        DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
+        instance.setType(frequency);
+        assertEquals(type, instance.getType(), "Midrange");
+    }
+
+    @Test
+    public void setTypeWoofer() {
+        double size = 6.5;
+        double frequency = 87.32;
+        String type = "Woofer";
+        DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
+        instance.setType(frequency);
+        assertEquals(type, instance.getType(), "Woofer");
     }
 
     @Test
     public void getBrand() {
+        double size = 25.4;
+        double frequency = 1501.5;
+        DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
+        assertEquals("Defualt brand is empty", instance.getBrand(), "");
     }
 
     @Test
     public void setBrand() {
+        double size = 25.4;
+        double frequency = 1501.5;
+        String brand = "Dayton";
+        DefaultSpeaker instance = new DefaultSpeaker(size, frequency);
+        instance.setBrand(brand);
+        assertEquals("Set brand to dayton", instance.getBrand(), brand);
     }
 }
