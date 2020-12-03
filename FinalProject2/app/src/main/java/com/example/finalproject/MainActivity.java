a7ad131b54fc77525e42d98c7ad6fd13c105a218
CRStromberg <Cstromberg86@gmail.com>
2020-12-02 17:56:11 -0700

    Data base about done

--- FinalProject2/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject2/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
 import android.widget.EditText;
 
@@ -16,7 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         part = (EditText)findViewById(R.id.partNum);
-
         helper = new myDbAdapter(this);
+
+    }
+
+    public void addPartNum(View view) {
+        String prtNum = part.getText().toString();
+
+        if(prtNum.isEmpty())
+        {
+            Message.message(getApplicationContext(),"Enter Part Number");
+        } else {
+            long id = helper.insertData(prtNum);
+        }
+        part.setText("");
+    }
+
+    Public void viewData(View view) {
+        String data = helper.getdata();
+        Message.message(this,data);
     }
 }
