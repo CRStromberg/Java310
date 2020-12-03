3e2ec21c4f849ad3eead5876c972a1878c30a4cc
root <Cstromberg86@gmail.com>
2020-12-02 17:12:46 -0700

    Started over

--- FinalProject2/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject2/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText part;
+    Button search;
+    myDbAdapter helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        part = (EditText)findViewById(R.id.partNum);
+
+        helper = new myDbAdapter(this);
     }
 }
